8e52134b45d9c143c5a0fb0025ee216bb6c6712f
Shradha <shradha.sgl@gmail.com>
2020-05-21 01:31:15 +0530

    Reviews slider added

---
+++ src/js/build-slider.js
@@ -0,0 +1,48 @@
+export default function slider() {
+    let currentIndex = 0,
+      timeout;
+  
+    const slides = [...document.querySelectorAll('.written')];
+  
+    console.log(slides);
+    for (let i = 0; i < slides.length; i++) {
+      document
+        .querySelector('.control-btns')
+        .appendChild(document.createElement('div'));
+    }
+    const dots = [...document.querySelectorAll('.control-btns > div')];
+  
+    function slide(n) {
+      slides[currentIndex].classList.remove('active');
+      dots[currentIndex].classList.remove('active');
+      slides[n].classList.add('active');
+      dots[n].classList.add('active');
+      currentIndex = n;
+  
+      timer();
+    }
+  
+    function nextSlide() {
+      return currentIndex + 1 >= slides.length ? 0 : currentIndex + 1;
+    }
+  
+    function prevSlide() {
+      return currentIndex - 1 <= 0 ? slides.length - 1 : currentIndex - 1;
+    }
+  
+    function timer() {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => slide(nextSlide()), 3000);
+    }
+  
+    // [...document.querySelectorAll('.controls > div')].forEach(e =>
+    //   e.addEventListener('click', e =>
+    //     e.target.classList.contains('next')
+    //       ? slide(nextSlide())
+    //       : slide(prevSlide())
+    //   )
+    // );
+  
+    slide(0);
+  }
+  
--- src/js/build.js
+++ src/js/build.js
@@ -1,1 +1,31 @@
 import '../sass/build.scss';
+import './nav';
+import slider from './build-slider';
+import reviews from '../static/data/build.json';
+
+for (let i = 0, j = reviews.length; i < j; i++) {
+
+    const written = document.createElement('div');
+    written.setAttribute('class', 'written');
+
+    const review = document.createElement('p');
+    review.setAttribute('class', 'review');
+    review.innerText = String(reviews[i].review);
+
+    const author = document.createElement('p');
+    author.setAttribute('class', 'author');
+    author.innerText = String(reviews[i].author);
+
+    const school = document.createElement('p');
+    school.setAttribute('class', 'institute');
+    school.innerText = String(reviews[i].school);
+
+    written.appendChild(review);
+    written.appendChild(author);
+    written.appendChild(school);
+    document.querySelector('.dimigo .galleryitems').appendChild(written);
+    //   console.log(review, author, school);
+}
+
+
+slider();
